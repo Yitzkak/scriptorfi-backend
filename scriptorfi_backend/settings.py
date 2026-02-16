cb023743e55857d2e00107346e1eb01ca9ca6e6b
Yitzkak <bevinchi@gmail.com>
2026-02-16 13:45:38 +0100

    changed cors order

--- scriptorfi_backend/settings.py
+++ scriptorfi_backend/settings.py
@@ -72,9 +72,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
